83132bddf458e18f37cafc796975f0127b558d31
jeanlf <jeanlf@63c20433-aa62-49bd-875c-5a186b69a8fb>
2007-06-20 18:12:55 +0000

    no message
    git-svn-id: svn://svn.code.sf.net/p/gpac/code/trunk@590 63c20433-aa62-49bd-875c-5a186b69a8fb

---
+++ gpac_www/bas.php
@@ -0,0 +1,1 @@
+<!-- =================== PIED DE LA PAGE ============================================  -->	<div id="Bas">		<p>(C) 2000-05 JLF / (C) 2005-0X ENST - Last update @ 07/2007 - <a href="mailto:jeanlf@users.sourceforge.net">Webmaster</a></p>	</div>
