354d6c8ef32efd3d30a0db398ad2f0e38b22aae2
jeanlf <jeanlf@63c20433-aa62-49bd-875c-5a186b69a8fb>
2007-08-29 17:55:47 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/gpac/code/trunk@644 63c20433-aa62-49bd-875c-5a186b69a8fb

--- gpac_www/doc_ismacryp.php
+++ gpac_www/doc_ismacryp.php
@@ -30,8 +30,9 @@
 
 <h1>Introduction</h1>
 <p>As of version 0.2.4, GPAC supports the ISMA E&amp;A specification, better known as ISMACryp. This specification provides reliable transmission of encrypted media data with key signaling and cryptographic resynchronization in case of packet loss or random access.</p> 
-<p>However, ISMACryp does not mandate anything regarding how keys are to be distributed (hereafter refered to as <i>KMS - Key Management System</i>) which is up to the content provider/distributor. 
-In other words, ISMA E&amp;A is concerned with cryptographic interoperability only, not rights management.</p>
+<p>As of version 0.4.4, GPAC supports the OMA DRM PDCF specification available <a href="http://www.openmobilealliance.org/release_program/docs/CopyrightClick.asp?pck=DRM&file=V2_0-20060303-A/OMA-TS-DRM-DCF-V2_0-20060303-A.pdf"=>here</a>. This specification is derived from the ISMA E&A specification and OMA DRM PDCF files have a structure almost equivalent to ISMA protected files. GPAC does NOT support the ROAP protocol and other tools from the OMA DRM framework, but has a dedicated module API (still at beta stage) allowing for pluging a decryption agent in the streaming core.</p>
+<p>Note that, unlike OMA DRM, ISMACryp does not mandate anything regarding how keys are to be distributed (hereafter refered to as <i>KMS - Key Management System</i>) which is up to the content provider/distributor. 
+In other words, ISMACryp is concerned with cryptographic interoperability only, not rights management.</p>
 <p>The current version of the specification uses for encryption the AES 128 bit algorithm in counter mode (AES-CTR). You do not need to know of all these things, the only thing you need to know 
 is that ISMACryp uses a 128 bit key and a 64 bit salt, which would need to be fetched by the client at some point for decryption.
 <br>In this document, the key and the salt will simply be refered to as <i>key</i> unless specified otherwise.</p>
@@ -116,6 +117,47 @@ mainly key information, KMS information, encryption instructions and eventually 
 <li>For GPAC client: key fetching will fail and the stream will be decoded WITHOUT being decrypted.</li>
 </ul>
 
+
+<br>
+<h1 ID="encrypt">OMA DRM authoring</h1>
+
+<p>In order to encrypt a 3GP/MP4 file into a PDCF file, MP4Box uses the same process as ISMA encryption, only the drm file syntax changes.</p>
+<p>Just like any XML file, the file must begin with the usual xml header. The file encoding SHALL BE UTF-8. The file is then a collection of <i>OMATrack</i> elements placed under an <i>OMADRM</i> document root (currently ignored). Each element describes all OMA DRM information needed to encrypt a given track. An <i>OMATrack</i> element may have children describing the optional textual headers defined in OMA DRM 2.0. Each textual header is inserted as is during OMA encryption, so be carefull not to specify twice the same header.</p>
+
+<h2>XML Syntax</h2>
+<table class="xml_app">
+<tbody><tr><td>&lt;OMADRM&gt;</td></tr>
+
+<tr><td>&lt;OMATrack trackID="..." key="..." selectiveType="..." rightsIssuerURL="..." contentID="..." transactionID="..." &gt;</td></tr>
+<tr><td>&lt;OMATextHeader&gt;textual header&lt;/OMATextHeader&gt;</td></tr>
+<tr><td>&lt;/OMATrack&gt;</td></tr>
+<tr><td>&lt;/OMADRM&gt;</td></tr>
+</tbody></table>
+
+<h2>Semantics</h2>
+<p>
+<ul>
+<li><i>trackID</i> : specifies the track ID to encrypt. This is a mandatory field, not specifing it will result in an error.</li>
+<li><i>key</i> : the AES-128 bit key to use. The key must be specified as an 32 bytes hex string, like <b>0x2b7e151628aed2a6abf7158809cf4f3c</b>. This is a mandatory field, not specifing it or using an improper length will result in an error.</li>
+<li><i>rightsIssuerURL</i> : the URL of the OMA DRM licence server. This is the URL to which an OMA client will request the keys using the ROAP protocol.</li>
+<li><i>contentID</i> : a string identifier for the content, passed during ROAP exchanges.</li>
+<li><i>transactionID</i> : a string identifier for the transaction, passed during ROAP exchanges.</li>
+<li><i>selectiveType</i> : specifies how selective encryption is to be used. The possible values are:
+<ul>
+<li><b>None</b> : no selective encryption, all samples encrypted (this is the default behavior).</li>
+<li><b>RAP</b> : only Random Access Samples (key frames) will be encrypted. If all media samples are RAPs, this defaults to <i>None</i>.</li>
+
+<li><b>Non-RAP</b> : only non-Random Access Samples (non-key frames) will be encrypted. If all media samples are RAPs, this defaults to <i>None</i>.</li>
+<li><b>Rand</b> : random selection of samples to encrypt is performed.</li>
+<li><b>X</b> : encrypts the first sample every <i>X</i> samples. <i>X</i> must be an integer greater than 2.</li>
+
+<li><b>RandX</b> : encrypts one random samples out of <i>X</i> samples. <i>X</i> must be an integer greater than 2.</li>
+</ul>
+</li>
+</ul>
+</p>
+<br/>
+
 <h1 ID="samples">Sample GPAC drm files</h1>
 <p>The following example shows how to encrypt a file with one track, using selective encryption of RAP samples, embedded keys and no IPMP signaling.</p>
 
@@ -141,15 +183,27 @@ mainly key information, KMS information, encryption instructions and eventually 
 <table class="xml_app">
 <tbody><tr><td>&lt;?xml version="1.0" encoding="UTF-8" &gt;</td></tr>
 <tr><td>&lt;ISMACryp&gt;</td></tr>
-<tr><td style="text-indent: 5%;">&lt;ISMACrypTrack trackID="1" key="0x2b7e151628aed2a6abf7158809cf4f3c" salt="0xf8f9fafbfcfdfeff" selectiveType="None" KMS_URI="https://gpac.sourceforget.net/kms/file.xml" /&gt;</td></tr>
+<tr><td style="text-indent: 5%;">&lt;ISMACrypTrack trackID="1" key="0x2b7e151628aed2a6abf7158809cf4f3c" salt="0xf8f9fafbfcfdfeff" selectiveType="None" KMS_URI="https://gpac.sourceforge.net/kms/file.xml" /&gt;</td></tr>
 <tr><td>&lt;/ISMACryp&gt;</td></tr>
 </tbody></table>
 
 
+
+<p>The following example shows how to encrypt a file with one track, without slective encryption, using OMA DRM.</p>
+
+<table class="xml_app">
+<tbody><tr><td>&lt;?xml version="1.0" encoding="UTF-8" &gt;</td></tr>
+<tr><td>&lt;OMADRM&gt;</td></tr>
+<tr><td style="text-indent: 5%;">&lt;OMATrack trackID="1" key="0x2b7e151628aed2a6abf7158809cf4f3c" selectiveType="None" rightsIssuerURL="https://gpac.sourceforge.net/kms" contentID="WatchMe1984" transactionID="14fd12zd3q" &gt;</td></tr>
+<tr><td>&lt;OMATextHeader&gt;Preview=instant;http://other.content.com/sonaive&lt;/OMATextHeader&gt;</td></tr>
+<tr><td>&lt;/OMATrack&gt;</td></tr>
+<tr><td>&lt;/OMADRM&gt;</td></tr>
+</tbody></table>
+
       </div>
 	</div>
 
-<?php $mod_date="\$Date: 2007-07-26 15:56:25 $"; ?><?php include_once("bas.php"); ?><!-- =================== FIN CADRE DE LA PAGE =========================================  -->
+<?php $mod_date="\$Date: 2007-08-29 17:55:47 $"; ?><?php include_once("bas.php"); ?><!-- =================== FIN CADRE DE LA PAGE =========================================  -->
 </div>
 </body>
 </html>
